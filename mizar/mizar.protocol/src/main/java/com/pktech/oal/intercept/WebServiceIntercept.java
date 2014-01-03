a727605205c3c5404bc69f5f63e6ee5e6d4f01a7
xuegongjian <xuegongjian@192.168.3.120>
2014-01-03 19:26:46 +0800

    bugfix

--- mizar/mizar.protocol/src/main/java/com/pktech/oal/intercept/WebServiceIntercept.java
+++ mizar/mizar.protocol/src/main/java/com/pktech/oal/intercept/WebServiceIntercept.java
@@ -21,17 +21,26 @@ public class WebServiceIntercept implements MethodInterceptor {
     }
 
     public Object invoke(MethodInvocation methodInvocation) throws Throwable {
-        String interfaceName = methodInvocation.getMethod().getDeclaringClass().getName();
+        // 由于接口是自动生成并发布的，直接取接口名称
+        String interfaceName = methodInvocation.getMethod().getDeclaringClass().getSimpleName();
+
         String methodName = methodInvocation.getMethod().getName();
         Map<String, String> params = new HashMap<String, String>();
         GatewayServiceInfo gatewayServiceInfo = CacheManager.getInstance().getServiceByInterfaceNameAndMethodName(interfaceName, methodName);
+        if (null == gatewayServiceInfo) {
+            return "系统找不到接口=" + interfaceName + "." + methodName;
+        }
         params.put(SERVICENAME, gatewayServiceInfo.getServiceName());
+
         if (null != methodInvocation.getArguments() && methodInvocation.getArguments().length > 0) {
             List<GatewayInParam> gatewayInParams = gatewayServiceInfo.getGatewayInParams();
             if (null == gatewayInParams || gatewayInParams.size() != methodInvocation.getArguments().length) {
-                return "error";
+                return "接口=" + interfaceName + "." + methodName + "要求的参数和传入的参数不匹配";
             }
             for (int i = 0; i < methodInvocation.getArguments().length; i++) {
+                if (null == methodInvocation.getArguments()[i]) {
+                    return "接口=" + interfaceName + "." + methodName + "的参数为null";
+                }
                 params.put(gatewayInParams.get(i).getParamName(), methodInvocation.getArguments()[i].toString());
             }
         }
